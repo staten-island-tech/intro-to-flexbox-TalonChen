8f2c324986fa91523807787d9b8c8cada34882f1
TalonChen <talonc@nycstudents.net>
2025-10-08 11:01:58 -0700

    dtr

--- index.js
+++ index.js
@@ -1,20 +1,71 @@
 const skins = [
-  { skinname: "Divergence", price: 8700, image: "images/divergence.webp" },
+  {
+    skinname: "Divergence",
+    price: 8700,
+    image: "images/divergence.jpg",
+    category: "vandal",
+  },
   {
     skinname: "Bubblegum Deathwish",
     price: 8700,
-    image: "images/bubblegum.webp",
+    image: "images/bubblegum.jpg",
+    category: "phantom",
   },
-  { skinname: "Doombringer", price: 8700, image: "images/doombringer.webp" },
-  { skinname: "Evori Dreamwings", price: 9900, image: "images/evori.webp" },
-  { skinname: "a", price: 0, image: "img" },
-  { skinname: "a", price: 0, image: "img" },
-  { skinname: "a", price: 0, image: "img" },
-  { skinname: "a", price: 0, image: "img" },
-  { skinname: "a", price: 0, image: "img" },
-  { skinname: "a", price: 0, image: "img" },
-  { skinname: "a", price: 0, image: "img" },
-  { skinname: "a", price: 0, image: "img" },
+  {
+    skinname: "Doombringer",
+    price: 8700,
+    image: "images/doombringer.jpg",
+    category: "phantom",
+  },
+  {
+    skinname: "Evori Dreamwings",
+    price: 9900,
+    image: "images/evori.jpg",
+    category: "vandal",
+  },
+  { skinname: "EX.O", price: 9500, image: "images/exo.jpg", category: "Knife" },
+  {
+    skinname: "Kuronami",
+    price: 9500,
+    image: "images/kuronami.jpg",
+    category: "Knife",
+  },
+  {
+    skinname: "Mystbloom",
+    price: 8700,
+    image: "images/mystbloom.jpg",
+    category: "Phantom",
+  },
+  {
+    skinname: "Singularity",
+    price: 6700,
+    image: "images/singularity.jpg",
+    category: "Vandal",
+  },
+  {
+    skinname: "SplashX",
+    price: 6700,
+    image: "images/splashx.jpg",
+    category: "vandal",
+  },
+  {
+    skinname: "Imperium",
+    price: 8700,
+    image: "images/imperium.jpg",
+    category: "knife",
+  },
+  {
+    skinname: "Nocturnum",
+    price: 8700,
+    image: "images/nocturnum.jpg",
+    category: "knife",
+  },
+  {
+    skinname: "Sovereign",
+    price: 7100,
+    image: "images/sovereign.jpg",
+    category: "knife",
+  },
   { skinname: "a", price: 0, image: "img" },
   { skinname: "a", price: 0, image: "img" },
   { skinname: "a", price: 0, image: "img" },
@@ -25,3 +76,11 @@ const skins = [
   { skinname: "a", price: 0, image: "img" },
   { skinname: "a", price: 0, image: "img" },
 ];
+
+//created array
+//find the container - querySelector
+//create function to add 1 card to screen
+// function inject(skins) {
+// out card on screen
+//insetadjacenthtml
+//}
